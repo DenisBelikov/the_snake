c1ab482179266d20fdfd8d2a9e762109f0db7890
DenisBelikov <DenisBelikov1992@yandex.ru>
2024-10-22 18:45:47 +0300

    Первоначальный коммит

--- tests/test_code_structure.py
+++ tests/test_code_structure.py
@@ -30,8 +30,8 @@ EXPECTED_APPLE_ATTRS = (
 
 
 def test_apple_inherits_from_game_object(_the_snake):
-    assert issubclass(_the_snake.Apple, _the_snake.Game_Object), (
-        'Класс `Apple` должен наследоваться от класса `GameOb_ject`.'
+    assert issubclass(_the_snake.Apple, _the_snake.GameObject), (
+        'Класс `Apple` должен наследоваться от класса `GameObject`.'
     )
 
 
