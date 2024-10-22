4e5e434e1ef7331d06fcc77ffba8f787bcae3e19
DenisBelikov <DenisBelikov1992@yandex.ru>
2024-10-22 21:19:49 +0300

    pip

--- the_snake.py
+++ the_snake.py
@@ -35,8 +35,8 @@ class GameObject:
 
     def draw(self, screen):
         """Рисование объекта на экране"""
-        raise NotImplementedError(
-        "Этот метод должен быть переопределён в подклассах")
+        raise NotImplementedError("Этот метод должен быть переопределён 
+                                  в подклассах")
 
 
 class Apple(GameObject):
@@ -87,7 +87,7 @@ class Snake(GameObject):
     def reset(self):
         """Сброс позиции и направления змеи при столкновении с самой
         собой. Сбросить состояние змеи до начального
-    """
+                                                    """
         center = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
         self.length = 1
         self.positions = [center]
