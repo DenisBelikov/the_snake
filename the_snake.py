4d5a9213484f1ca7ac65f2356e43463d0dc4d182
DenisBelikov <DenisBelikov1992@yandex.ru>
2024-10-23 18:13:38 +0300

    PEP 8

--- the_snake.py
+++ the_snake.py
@@ -158,7 +158,7 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
     pygame.display.set_caption('Змейка')
-    clock = pygame.time.Clock()
+    clock = pygame.time.clock()
 
     snake = Snake()
     apple = Apple()
